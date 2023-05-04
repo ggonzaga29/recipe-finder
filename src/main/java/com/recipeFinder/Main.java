01016c1343e87813c0d21a087faed6c04b5c2332
Gian Gonzaga <gonzagagianfrancesco@gmail.com>
2023-05-04 09:45:01 +0800

    initial commit

---
+++ src/main/java/com/recipeFinder/Main.java
@@ -0,0 +1,38 @@
+package com.recipeFinder;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Connection connection = null;
+        try {
+            // create a database connection
+            connection = DriverManager.getConnection("jdbc:sqlite:db/recipeFinder.db");
+            Statement statement = connection.createStatement();
+
+            ResultSet rs = statement.executeQuery("select * from person");
+            while (rs.next()) {
+                // read the result set
+                System.out.println("name = " + rs.getString("name"));
+                System.out.println("id = " + rs.getInt("id"));
+            }
+        } catch (SQLException e) {
+            // if the error message is "out of memory",
+            // it probably means no database file is found
+            System.err.println(e.getMessage());
+        } finally {
+            try {
+                if (connection != null)
+                    connection.close();
+            } catch (SQLException e) {
+                // connection close failed.
+                System.err.println(e.getMessage());
+            }
+        }
+    }
+}
---
+++ src/main/java/com/recipeFinder/db/DBHandler.java
@@ -0,0 +1,46 @@
+package com.recipeFinder.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class DBHandler {
+    private Connection connection;
+
+    public void connect() throws SQLException {
+        connection = DriverManager.getConnection("jdbc:sqlite:db/recipeFinder.db");
+    }
+
+    public void disconnect() throws SQLException {
+        if (connection != null) {
+            connection.close();
+        }
+    }
+
+    public void executeUpdate(String sql) throws SQLException {
+        Statement statement = null;
+        try {
+            statement = connection.createStatement();
+            statement.executeUpdate(sql);
+        } finally {
+            if (statement != null) {
+                statement.close();
+            }
+        }
+    }
+
+    public ResultSet executeQuery(String sql) throws SQLException {
+        Statement statement = null;
+        try {
+            statement = connection.createStatement();
+            return statement.executeQuery(sql);
+        } catch (SQLException e) {
+            if (statement != null) {
+                statement.close();
+            }
+            throw e;
+        }
+    }
+}
